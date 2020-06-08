b815f2509811203b379d7ee00e542fab8eb0d1a0
Dmitriy Petrov <dmtr.petrov@icloud.com>
2020-06-08 16:30:00 +0600

    Add new fonts and assets

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -11,7 +11,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
 }
