d0887c8997884e69cebf6c608d75c38e28831b41
Dmitriy Petrov <dmitriy.petrov@bytepace.com>
2020-06-08 15:40:54 +0600

    First commit

---
+++ WeatherApp/Application/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Dmitriy Petrov on 08.06.2020.
+//  Copyright © 2020 Dmitriy Petrov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
