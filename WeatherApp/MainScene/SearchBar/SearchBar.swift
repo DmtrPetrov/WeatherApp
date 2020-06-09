4958baaf45cd45ea87ac060f48b3a5e0bd06f721
Dmitriy Petrov <dmtr.petrov@icloud.com>
2020-06-09 11:21:33 +0600

    Add toolbar for searchBar

--- WeatherApp/MainScene/SearchBar/SearchBar.swift
+++ WeatherApp/MainScene/SearchBar/SearchBar.swift
@@ -17,6 +17,8 @@ class SearchBar: UIView {
     @IBOutlet weak var okButton: UIButton!
     
     private func setupUI() {
+        setupToolBar()
+        
         layer.masksToBounds = true
         layer.cornerRadius = 4
         layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
@@ -24,6 +26,18 @@ class SearchBar: UIView {
         layer.shadowRadius = 8
         layer.shadowOpacity = 1
         layer.shadowOffset = CGSize(width: 0, height: 2)
+    }
+    
+    private func setupToolBar() {
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: searchTextField.frame.width, height: 44.0))
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Отмена", style: .plain, target: self, action: #selector(cancel))
+        toolBar.setItems([cancelButton, flexible], animated: false)
+        searchTextField.inputAccessoryView = toolBar
+    }
+    
+    @objc private func cancel() {
+        searchTextField.resignFirstResponder()
     }
 }
 
